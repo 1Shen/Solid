72612b730935d711741c31529e638ce8be197655
wy <1627058473@qq.com>
2019-04-11 19:22:06 +0800

    账号提示

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,7 +15,7 @@
                         <!-- {{ __('E-Mail Address') }} -->用户名</label>
 
                     <div class="layui-input-block">
-                        <input id="username" type="text" class="layui-input layui-form-danger {{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="" required autofocus placeholder="请输入用户名">
+                        <input id="username" type="text" class="layui-input layui-form-danger {{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="TEA0316" required autofocus placeholder="请输入用户名">
                         @if ($errors->has('username'))
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $errors->first('username') }}</strong>
@@ -32,7 +32,7 @@
                         <!-- {{ __('Password') }} -->密码</label>
 
                     <div class="layui-input-block">
-                        <input id="password" type="password" class="layui-input layui-form-danger {{ $errors->has('password') ? ' is-invalid' : '' }}" value="" name="password" required placeholder="请输入密码">
+                        <input id="password" type="password" class="layui-input layui-form-danger {{ $errors->has('password') ? ' is-invalid' : '' }}" value="123456789" name="password" required placeholder="请输入密码">
                         @if ($errors->has('password'))
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $errors->first('password') }}</strong>
@@ -72,21 +72,17 @@
     $(function() {
         $('#login-nav').addClass('nav-active');
 
-        // swal({
-        //     title: '请先登录',
-        //     text: "<div>游客账号：admin</div><div>游客密码：123456789</div>",
-        //     type: 'warning',
-        //     confirmButtonColor: '#3085d6',
-        //     confirmButtonText: '确定',
-        // }).then(function(isConfirm) {
-        //     if (isConfirm) {
-        //         placeTopOnMiddle();
-        //         closeMagnifier();
+        swal({
+            title: '请先登录',
+            text: "<div>教师账号：TEA0316</div><div>教师密码：123456789</div><div>学生账号：STU0316</div><div>学生密码：123456789</div>",
+            type: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: '确定',
+        }).then(function(isConfirm) {
+            if (isConfirm) {
 
-        //         stepComplete(3, 1);
-        //         missionComplete(mission_step);
-        //     }
-        // });
+            }
+        });
     });
 </script>
 @endsection 
